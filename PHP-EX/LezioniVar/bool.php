fa0f1a04cb60b7baf454cbbf56d51b37e9bdf476
ArmandoAnescu <121225784+ArmandoAnescu@users.noreply.github.com>
2024-06-13 16:02:46 +0200

    finiti i bool(var)

--- PHP-EX/LezioniVar/bool.php
+++ PHP-EX/LezioniVar/bool.php
@@ -12,8 +12,12 @@ $verify =false;
 // if($verify===true)
 // $verify=4>5;una var bool può essere direttamente una espressione che poi ritorna t o f
 $verify=4===5;
+$verify=(bool)"Hellow World";//il risultato sarà t perchè la str non è ne vuota ne "0"
+var_dump($verify);//dice il tipo della var inserita.
 if($verify){
     echo "vero";
 }else{
     echo "falso";
 }
+//quando passi i dati da un form non passare True o false come condizioni perchè li converte in stringa e risultano entrambi true 
+//perchè le stringhe sono non vuote, quindi è meglio utilizzare 0 e 1 come valori booleani
