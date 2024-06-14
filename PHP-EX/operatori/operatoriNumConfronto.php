61b399a495e3a7808d41e9abd8fc36a158eb184d
ArmandoAnescu <121225784+ArmandoAnescu@users.noreply.github.com>
2024-06-14 11:46:56 +0200

    spaceship and null coalescing operator

--- PHP-EX/operatori/operatoriNumConfronto.php
+++ PHP-EX/operatori/operatoriNumConfronto.php
@@ -16,8 +16,30 @@ $c=$a==$b;//torna true
 $c=$a===$b;//torna false
 var_dump($a,$b,$c);
 $price=0;
-if($price){
-    echo "il prezzo aggiornato";
+if($price!==null){//controllo che il tipo sia div da NULL
+    echo "il prezzo è aggiornato";
 }else{
     echo "il prezzo non è aggiornato";
 }
+$data=[4,5,0];
+var_dump(in_array(0,$data));//controllo se il valore 0 è presente nell'array
+/*
+$a==$b true se i val sono uguali
+$a===$b true se i val e i tipi sono uguali
+$a!=$b diverso se a diverso da b
+$a <> $b diverso se a diverso da b
+$a !== $b Non identici True se a e b sono divresi sia in tipo che valore
+$a<$b true se è minor stretto di b
+$a>$b true se a è maggiore stretto di b
+$a<=$b true se a è minore o uguale a b
+$a>=$b true se a è maggiore o uguale a b
+$a <=> $b 
+Ritorna 0 se a e b sono uguali
+Ritorna 1 se a>b
+Ritorna -1 se a è < di b
+$a ?? $b ?? $c  ?? $d ritorna il primo valore che non è null
+
+*/
+$ar=['name'=>'BB','city'=>'Turkmenia','lastname'=>null];
+$ar['lastname']??='N/A';//se null metti N/A
+echo $ar['lastname'];
