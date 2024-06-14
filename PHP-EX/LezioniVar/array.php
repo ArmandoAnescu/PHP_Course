658d053ecaae4f073c4992a119925815e037d1db
ArmandoAnescu <121225784+ArmandoAnescu@users.noreply.github.com>
2024-06-14 09:04:45 +0200

    inizio array

---
+++ PHP-EX/LezioniVar/array.php
@@ -0,0 +1,14 @@
+<?php
+/*
+In php ai valori degli array puoi dare indici numerici o stringhe
+gli array possono essere usati come, liste
+funzioni già native in php 🤑
+*/
+$arr=array();
+//$arr=[];puoi usare anche questa sintassi scegli te
+$arr=array('RED','GREEN','BLUE');
+//$arr=['RED','GREEN','BLUE'];
+$arr[]='PINK';//aggiunge un val
+//var_dump($arr);
+print_r($arr);//la diff da var dump oltre a come lo stampa è che qua mostra l'indice e
+//il valore contenuto mentre var dump ti dice anche il tipo el valore contenuto nell'array
--- PHP-EX/LezioniVar/stringhe.php
+++ PHP-EX/LezioniVar/stringhe.php
@@ -22,24 +22,44 @@ $account[0]
 EOD;//<<<Identifiativo identificativo; posso mettere dentro tutti i dati in una stringa come voglio senza stare a preoccuparmi degli apici
 //posso mettere qunati tag volgio , puoi metterci anche una poesia heredoc
 //array e oggetti possono essere messi dentro
-echo $dati;
+echo $dati.'<br>';
 //nl2br= new line to breaktrasforma \n in un break
 //nowdoc diff di here doc è che metti gli ' '
-$data2=<<<'EOD'
-il mio nome é $name <br>
+// echo<<<'EOT'
+// il mio nome é $name <br>
 
-il mio cognome è $lastName
+// il mio cognome è $lastName
 
-il mio indirizzo è $address <br>
+// il mio indirizzo è $address <br>
 
-$account[SSN] <br>
+// $account[SSN] <br>
 
-{$account['SSN']} <br>
+// {$account['SSN']} <br>
 
-$obj->name
+// $obj->name
 
-$account[0]
-EOD;//non parsifica nulla lascia tutto come lo scrivi es: $var=2 sarà in output $var non il valor come con heredoc
+// $account[0]
+// EOT;//non parsifica nulla lascia tutto come lo scrivi es: $var=2 sarà in output $var non il valor come con heredoc
+$surName="Mogger";
+$char='à';
+echo strlen($char);//ritorna il num di byte della stringa
+/*
+la à occupa 2 byte anche se la sua lunghezza è 1 perciò se la sostituite nel mezzo di una stringa vi verra fuori
+un char con simbolo sconosciuto perchè occupa 2 byte nn uno
+*/
+echo mb_strlen($char);//ritorna la lunghezza della stringa/char
+/*
+Convertiti in stringa:
+null=''
+true='1'
+false=''
+i numeri rimangono tali
+*/
+$bool=True;
+$bool=false;
+var_dump((string)$bool);
+echo $bool;
+echo $surName." Mogximus";//si usa il . per concatenare stringhes
 ?>
 </h1>
 <!--per scrviere un elemento di un array che usa una key o togli ' ' o metti {array[key]} altrimenti è errore
