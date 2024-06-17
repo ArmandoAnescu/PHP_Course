5b71ee537940d166ceedcf3eaa511a8f7d072d8d
ArmandoAnescu <121225784+ArmandoAnescu@users.noreply.github.com>
2024-06-17 10:13:55 +0200

    fatto union type e finite le funzioni iniziate funz str
    finito l'union type e le arrow function finendo il cap delle funzioni inizio funzioni stringhe

--- PHP-EX/Funzioni/uniontypes.php
+++ PHP-EX/Funzioni/uniontypes.php
@@ -1,1 +1,20 @@
 <?php
+declare(strict_types=1);
+/*
+Union types permette di specificare più tipi per un parrametro
+* type|type
+* int | float
+
+
+*/
+// function somma(int $a, int $b):int{
+//     return $a + $b;
+// }
+// echo somma(5.5,6);//questo senza union types causa errore
+function somma2(int|float|string $a=null,int|float  $b):int|float|null{
+    return $a + $b;
+}
+echo somma2(5.5,6);
+class B{
+    public string |Stringable $nome;
+}//Stringable indica l'implementazione del metodo ToString()
