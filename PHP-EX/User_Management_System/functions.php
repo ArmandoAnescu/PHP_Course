6180dec644cfd7953ffe317d582f40aa6e5ddeea
ArmandoAnescu <121225784+ArmandoAnescu@users.noreply.github.com>
2024-06-20 10:32:08 +0200

    Create funzioni user rand
    Create e testate le funzioni per creare e inserire user random

--- PHP-EX/User_Management_System/functions.php
+++ PHP-EX/User_Management_System/functions.php
@@ -1,4 +1,9 @@
 <?php
+
+require_once 'connection.php';
+/*
+funzione per generare utenti random
+*/
 function getRandName(){
     $names=[
         'John',
@@ -38,13 +43,15 @@ function getRandName(){
         'Zadra',
         'Targa',
         'Gasparini'
-    ];
+    ];//dopo una scelta di nomi e cognomi comuni per puro caso
     return $names[mt_rand(0,count($names)-1)].' '.$lastnames[mt_rand(0,count($lastnames)-1)];
+    //ritorno una scelta random del nome concatenato al cognome e uno ' ' in mezzo
 }
 // echo getRandName();
-// echo getRandEmail(getRandName()); 
-echo getRandFiscalCode();
-function getRandEmail($name){
+// // echo getRandEmail(getRandName()); 
+// echo getRandFiscalCode();
+// echo getRandAge();
+function getRandEmail($name){//per l'email passo il nome metto un num rand la @ e poi un dominio usando rand
     $domains=[
         'google.com',
         'yahoo.com',
@@ -55,15 +62,36 @@ function getRandEmail($name){
         'hotmail.it'
     ];
     return strtolower(str_replace(' ','.',$name).mt_rand(10,99).'@'. $domains[mt_rand(0,count($domains)-1)]); 
-}
+}//mt_rand ha estremo incluso perciò ricordate di togliere -1
 
 function getRandFiscalCode(){
     $res='';
     for($i=16;$i>0;$i-=1){
-        $res .=chr(mt_rand(65,90));
+        $res .=chr(mt_rand(65,90));//genero 16 lettere maiusc a caso
     }
     return $res;
 }
 
+function getRandAge(){
+    return mt_rand(16,50);//ritorno un num nell'intervallo
+}
+function insertRandUser($totale,mysqli $mysqli){
+    $i=$totale;
+    while($i>0){
+        $username=getRandName();
+        $email=getRandEmail($username);
+        $age=getRandAge();
+        $fiscalcode=getRandFiscalCode();//genero i dati
+        $sql='INSERT INTO users (username,email,fiscalcode,age) VALUES ';//ricorda lo spazio altrimenti hai errore
+        $sql.="('$username','$email','$fiscalcode',$age);";//uso i comandi di MYSQL e poi concateno il resto
+        $res=$mysqli->query($sql);//chiedo alla mia connessione al DB una query per aggiungere il nuovo utente
+        if(!$res){
+            echo $mysqli->error;
+        }else{
+            $i--;
+        }
+    }
+}
+insertRandUser(30,$mysqli);
 
 
